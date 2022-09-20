ddb9b85058fabfebd6ee677e06d26978082c6fa7
EPJrWorld <edwin.przezwiecki.jr@icloud.com>
2022-09-20 13:07:42 +0200

    Completion

---
+++ Project16/Capital.swift
@@ -0,0 +1,21 @@
+//
+//  Capital.swift
+//  Project16
+//
+//  Created by Edwin Przeźwiecki Jr. on 20/09/2022.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
